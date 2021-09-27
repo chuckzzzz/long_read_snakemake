4c3b2a2aaefc8d050c48b643a53f3d88d2338b82
Ziyang Zhang <zzj4347@quser23.quest.it.northwestern.edu>
2021-09-27 00:27:00 -0500

    modularize calculation

--- check_ont_sv.py
+++ check_ont_sv.py
@@ -1,6 +1,6 @@
 import pysam
-file1="my.sam"
-file2="xt.sam"
+file1="/projects/b1042/YueLab/zzhang/ont_test/variants/variants.vcf" #my
+file2="/projects/b1042/YueLab/xtwang/workspace/Cancer-nanopore/SCaBER-MD/SCaBER_nanopore.sniffles.vcf" #xiaotao's
 def extract_coords_from_sam(sam_file):
     pos_by_chrom={}
     
@@ -18,14 +18,20 @@ def extract_coords_from_sam(sam_file):
 my=extract_coords_from_sam(file1)
 xt=extract_coords_from_sam(file2)
 
-total=0
-overlapped=0
-interval=0
-for chromosome, positions in my.items():
-    xt_positions=xt[chromosome]
-    for cur_pos in positions:
-        total+=1
-        _,closest_pos=min(enumerate(xt_positions), key=lambda x: abs(x[1]-cur_pos))
-        if(abs(closest_pos-cur_pos)<interval+1):
-            overlapped+=1
-print("%.1f %%of my SV overlaps with Xiaotao's SV with %dbp interval. "%(((overlapped/total)*100), interval))
+def calculate_overlap(interval=5):
+    total=0
+    overlapped=0
+    for chromosome, positions in my.items():
+        xt_positions=xt[chromosome]
+        for cur_pos in positions:
+            total+=1
+            _,closest_pos=min(enumerate(xt_positions), key=lambda x: abs(x[1]-cur_pos))
+            if(abs(closest_pos-cur_pos)<interval+1):
+                overlapped+=1
+    print("%.1f %%of my SV overlaps with Xiaotao's SV with %dbp interval. "%(((overlapped/total)*100), interval))
+
+calculate_overlap(interval=10)
+calculate_overlap(interval=5)
+calculate_overlap(interval=0)
+
+
